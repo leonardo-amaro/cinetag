6ccc036b9370991cd5314f1b513b17b0bf8db345
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-04-28 11:45:17 -0300

    resolvendo desafio #1

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./Rodape.module.css";
+
+const Rodape = () => {
+  return (
+    <footer className={styles.rodape}>
+      <h2>
+        Criado por Alura.
+      </h2>
+    </footer>
+  );
+}
+
+export default Rodape;
