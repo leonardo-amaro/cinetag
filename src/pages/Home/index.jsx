1196f5e1ec8da5d4d8e3b03455cbcaee34b45e7d
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-04-29 08:49:25 -0300

    criando componente Banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import Cabecalho from "@/components/Cabecalho";
 import Rodape from "@/components/Rodape";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
     <>
       <Cabecalho />
+      <Banner imagem="home" />
       <Rodape />
     </>
   );
