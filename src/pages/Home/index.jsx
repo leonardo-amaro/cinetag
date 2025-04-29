00f2e05724a737b3a955133b4c7006751bc60dfd
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-04-29 08:54:59 -0300

    criando componente Titulo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,16 @@
 import Cabecalho from "@/components/Cabecalho";
 import Rodape from "@/components/Rodape";
 import Banner from "@/components/Banner";
+import Titulo from "@/components/Titulo";
 
 export default function Home() {
   return (
     <>
       <Cabecalho />
       <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
       <Rodape />
     </>
   );
