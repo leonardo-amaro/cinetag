847fc8f44a09dc0eb203cb051bacfd3c5a71fa5f
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-04-29 09:32:33 -0300

    criando rota Favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cabecalho from "./components/Cabecalho";
+import Rodape from "./components/Rodape";
 import Home from "./pages/Home";
+import Favoritos from "./pages/Favoritos";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
+      <Cabecalho />
       <Routes>
         <Route index element={ <Home /> } />
+        <Route path="favoritos" element={ <Favoritos /> } />
       </Routes>
+      <Rodape />
     </BrowserRouter>
   );
 }
