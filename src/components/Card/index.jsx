995e89965b54ecfd67be424edd23276fcc14530d
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-04-30 09:31:00 -0300

    tornando rota acessível

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
 import iconeDesfavoritar from "./favorite.png";
 import iconeFavoritar from "./favorite_outline.png";
@@ -9,8 +10,10 @@ const Card = ({ id, titulo, capa }) => {
 
   return (
     <div className={styles.container} key={id}>
-      <img src={capa} alt={titulo} className={styles.capa} />
-      <h2>{titulo}</h2>
+      <Link to={`/player/${id}`}>
+        <img src={capa} alt={titulo} className={styles.capa} />
+        <h2>{titulo}</h2>
+      </Link>
       <img 
         src={icone}
         alt="Botão de favoritar filme"
--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -2,6 +2,7 @@ import Banner from "@/components/Banner";
 import Titulo from "@/components/Titulo";
 import videos from "@/json/db.json";
 import { useParams } from "react-router-dom";
+import styles from "./Player.module.css";
 
 export default function Player() {
   const { id } = useParams();
@@ -13,15 +14,17 @@ export default function Player() {
       <Titulo>
         <h1>Player</h1>
       </Titulo>
-      <iframe 
-        width="560" 
-        height="315" 
-        src={video.link} 
-        title={video.titulo} 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerPolicy="strict-origin-when-cross-origin" 
-        allowFullScreen
-      ></iframe>
+      <section className={styles.container}>
+        <iframe 
+          width="560" 
+          height="315" 
+          src={video.link} 
+          title={video.titulo} 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerPolicy="strict-origin-when-cross-origin" 
+          allowFullScreen
+        ></iframe>
+      </section>
     </>
   );
 }
